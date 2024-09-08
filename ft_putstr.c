a5342aefc86b1c346ba067a8a332d0dc8b25bbe8
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-08 18:00:53 +0200

    Update ft_putstr.c

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 17:55:23 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/08 17:58:51 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/08 18:00:53 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,9 @@ void	ft_putstr(char *str)
 	while (*str)
 		write(1, str++, 1);
 }
-
-int     ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
 /*
-int	main(void)
+int	main()
 {
 	ft_putstr("HELLO BITCH!\n");
-	return (0);
 }
 /
