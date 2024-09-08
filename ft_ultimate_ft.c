ee39887ccd38b262dfcd60ac5ceb9ea7a7c07026
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-08 17:40:04 +0200

    Update ft_ultimate_ft.c

--- ft_ultimate_ft.c
+++ ft_ultimate_ft.c
@@ -6,8 +6,44 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 17:33:25 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/08 17:33:37 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/08 17:40:03 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft.h"
+
+void    ft_ultimate_ft(int *********nbr)
+{
+       *********nbr = 42; 
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int	i;
+	int	*ptr1;
+	int	**ptr2;
+	int	***ptr3;
+	int	****ptr4;
+	int	*****ptr5;
+	int	******ptr6;
+	int	*******ptr7;
+	int	********ptr8;
+	int	*********ptr9;
+
+	i = 1;
+	ptr1 = &i;
+	ptr2 = &ptr1;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	ptr9 = &ptr8;
+	printf("%i\n", i);
+	ft_ultimate_ft(ptr9);
+	printf("%i\n", i);
+	return (0);
+}
+*/ 
