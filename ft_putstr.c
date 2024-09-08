2ccaf4532f7ecf3ca78f35dc8aa2639f0029fbbe
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-08 18:03:24 +0200

    Update ft_putstr.c

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,11 +6,21 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 17:55:23 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/08 18:00:53 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/08 18:03:23 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+int     ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
 
 void	ft_putstr(char *str)
 {
