3ba50bc1db8ea5d9cbf6632cb46bdd5fa17762c1
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-08 18:30:12 +0200

    Update ft_strncpy.c

---
+++ ft_strncpy.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 18:29:44 by csauron           #+#    #+#             */
+/*   Updated: 2024/09/08 18:30:11 by csauron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft.h"
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+/*
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	main(void)
+{
+	char	*src;
+	char	dest[0];
+
+	src = "test";
+	printf("%s\n", ft_strncpy(dest, src, ft_strlen(src)));
+}*/
