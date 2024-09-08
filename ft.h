403cf34b6c7b613e878755da1b11ea7511e7db95
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-08 17:09:19 +0200

    Update ft.h

---
+++ ft.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 00:59:13 by csauron           #+#    #+#             */
+/*   Updated: 2024/09/08 17:09:19 by csauron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void    ft_putchar(char c);
+void    ft_print_alphabet();
+void    ft_print_reverse_alphabet();
+void    ft_print_numbers();
+void    ft_is_negative(int n);
+void    ft_prnt_rslt(char x, char y, char z);
+void    ft_print_comb();
+void	ft_print_comb2(void);
+void	ft_putnbr(int nb);
+void    ft_combn(int n, char c,char *tab);
+void    ft_print(char *tab, char c);
+void	ft_print_combn(int n);
+void    ft_ft(int *nbr);
+
+#endif
