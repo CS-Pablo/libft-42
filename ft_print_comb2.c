bbb7f18872f4d5ee80eb6017575d5da545b06c6c
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-08 17:23:54 +0200

    Update ft_print_comb2.c

---
+++ ft_print_comb2.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 17:23:49 by csauron           #+#    #+#             */
+/*   Updated: 2024/09/08 17:23:54 by csauron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void	ft_print_result(int i, int j)
+{
+	ft_putchar('0' + (i / 10));
+	ft_putchar('0' + (i % 10));
+	ft_putchar(' ');
+	ft_putchar('0' + (j / 10));
+	ft_putchar('0' + (j % 10));
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i++ <= 98)
+	{
+		j = i;
+		while (j++ <= 99)
+		{
+			if (!(i == 1 && j == 2))
+				write(1, ", ", 2);
+			ft_print_result(i - 1, j - 1);
+		}
+	}
+}
+
+int     main()
+{
+    ft_print_comb2();
+}
