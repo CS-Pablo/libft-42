d8d263396d2f2ff55e5d61666a064d8e47d252c0
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-08 18:17:11 +0200

    Update ft_sort_int_tab.c

---
+++ ft_sort_int_tab.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 18:16:41 by csauron           #+#    #+#             */
+/*   Updated: 2024/09/08 18:17:10 by csauron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft.h"
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	tmp;
+
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size - 1)
+		{
+			if (tab[j] > tab[j + 1])
+			{
+				tmp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(void)
+{
+	int	tab[] = {9, 1, 7, 3, 6, 4, 8, 5, 2, 0};
+	ft_print_int_tab(tab, 10);
+	printf("\n");
+	ft_sort_int_tab(tab, 10);
+	ft_print_int_tab(tab, 10);
+	printf("\n");
+	return (0);
+}
