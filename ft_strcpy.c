adf5c65af9206398509961bba3574adf0dea1918
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-08 18:31:51 +0200

    Update ft_strcpy.c

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,7 +6,31 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 18:23:23 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/08 18:23:35 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/08 18:31:51 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft.h"
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char	dest[0];
+	char	*src = "Test";
+	printf("src: %s\ndest(0): %s\n", src, dest);
+	printf("dest(1): %s\n", ft_strcpy(dest, src));
+}*/
