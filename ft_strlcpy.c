032603b93942d4546631f6eaff0866fe5cbcbb5f
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-08 19:13:40 +0200

    Update ft_strlcpy.c

---
+++ ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 19:12:43 by csauron           #+#    #+#             */
+/*   Updated: 2024/09/08 19:13:40 by csauron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (size > 0)
+	{
+		size -= 1;
+		while (src[i] && i < size)
+		{
+			if (i < size)
+				dest[i] = src[i];
+			i++;
+		}
+		dest[i] = 0;
+	}
+	while (src[i])
+			i++;
+	return (i);
+}
