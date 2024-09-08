2c687c3e8a30444e0d14493c8adfa62331769a0d
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-08 17:53:00 +0200

    Update ft_ultimate_div_mod.c

---
+++ ft_ultimate_div_mod.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 17:52:49 by csauron           #+#    #+#             */
+/*   Updated: 2024/09/08 17:52:59 by csauron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	div;
+	int	mod;
+
+	div = *a / *b;
+	mod = *a % *b;
+	*a = div;
+	*b = mod;
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int	a;
+	int	b;
+
+	a = 20;
+	b = 12;
+	printf("a=%i\nb=%i\n", a, b);
+	ft_ultimate_div_mod(&a, &b);
+	printf("a/b=%i\na%%b=%i\n", a, b);
+	return (0);
+}
+*/
