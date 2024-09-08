44560542687c3fd68d4680e9c71cb14ffdba6fa5
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-08 17:57:49 +0200

    Update ft.h

--- ft.h
+++ ft.h
@@ -6,7 +6,7 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 00:59:13 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/08 17:54:05 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/08 17:57:48 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,6 @@ void	ft_swap(int *a, int *b);
 void    ft_ultimate_ft(int *********nbr);
 void	ft_div_mod(int a, int b, int *div, int *mod);
 void	ft_ultimate_div_mod(int *a, int *b);
+void	ft_putstr(char *str);
 
 #endif
