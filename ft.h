1a0be29de3e197e5c7b75f17e15e3aa7f4d3138e
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-08 19:10:28 +0200

    Update ft.h

--- ft.h
+++ ft.h
@@ -6,7 +6,7 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 00:59:13 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/08 19:06:03 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/08 19:10:27 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,7 @@ int	    ft_str_is_lowercase(char *str);
 int	    ft_str_is_uppercase(char *str);
 int	    ft_str_is_printable(char *str);
 char	*ft_strupcase(char *str);
+char	*ft_strlowcase(char *str);
 
 //C03
 
