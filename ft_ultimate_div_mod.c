f601bae38d55b942094864c00ae1ecfd26152ac7
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-09 16:27:36 +0200

    Update ft_ultimate_div_mod.c

--- ft_ultimate_div_mod.c
+++ ft_ultimate_div_mod.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sauron <sauron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 17:52:49 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/08 18:45:27 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/09 16:27:35 by sauron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft.h"
+#include "libft.h"
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
