a1faf68c2036b15bf46eeabe4dc5bab1ae5929e9
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-09 16:20:17 +0200

    Update ft_ft.c

--- ft_ft.c
+++ ft_ft.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ft.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sauron <sauron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 15:02:08 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/08 17:23:07 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/09 16:20:17 by sauron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft.h"
+#include "libft.h"
 
 void    ft_ft(int *nbr)
 {
