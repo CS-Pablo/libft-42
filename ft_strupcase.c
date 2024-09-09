6f1550be02d2fb44c9232266f1a449d25a3248f2
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-09 16:27:51 +0200

    Update ft_strupcase.c

--- ft_strupcase.c
+++ ft_strupcase.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strupcase.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sauron <sauron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 19:04:39 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/08 19:05:18 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/09 16:27:50 by sauron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft.h"
+#include "libft.h"
 
 char	*ft_strupcase(char *str)
 {
