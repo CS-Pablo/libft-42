5a347d80af222777a6488f72fb161f115fecebc5
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-09 16:25:24 +0200

    Update ft_str_is_numeric.c

--- ft_str_is_numeric.c
+++ ft_str_is_numeric.c
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sauron <sauron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 18:44:18 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/08 18:46:06 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/09 16:25:23 by sauron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft.h"
-
+#include "libft.h"
 
 int     ft_str_is_numeric(char *str)
 {
